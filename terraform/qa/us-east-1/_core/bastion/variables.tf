0368a0c7e83d1d660033561a23d0cc2d83df71c5
Robert Geiner <robert.geiner@weather.com>
2017-07-13 11:40:45 -0400

    add billing code to bastion.... enven that it's not currently accepted by the module

--- terraform/qa/us-east-1/_core/bastion/variables.tf
+++ terraform/qa/us-east-1/_core/bastion/variables.tf
@@ -3,7 +3,7 @@ variable "chef_version" {
 }
 
 variable "additional_ips" {
-   default = "71.0.127.137/32"  # rob geiner home
+   default = "71.0.127.137/32,52.2.245.178/32"  # rob geiner home and jenkins_sun
 }  # dev/devops home static ips
 
 variable "chef_run_list" {
