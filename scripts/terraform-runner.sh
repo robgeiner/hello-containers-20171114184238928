2553c36e5fb9972d68ede0f5a92c07989262e2d8
Robert Geiner <robert.geiner@weather.com>
2017-07-27 16:20:34 -0400

    backout

--- scripts/terraform-runner.sh
+++ scripts/terraform-runner.sh
@@ -70,6 +70,8 @@ echo "Step 1: Get Credentials:"
 
 if [[ $OSTYPE != darwin* ]]; then
     echo "Info: I'm running from an EC2 Instance, I'll get my credentials from my meta-data url."
+    export AWS_ACCESS_KEY=${AWS_ACCESS_KEY_ID}
+    export AWS_SECRET_ACCESS=${AWS_SECRET_ACCESS_KEY}
     export aws_secret_access_id=${AWS_ACCESS_KEY_ID}
     export aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
     export AWS_PROFILE_NAME=
