b1dfd8363d4dca55044740abd14f48ce985fad73
Jenkins <sunsystemsengineeringgroup@weather.com>
2017-08-23 18:59:51 +0000

    Update Image ENV:dev REGION:us-east-1 SERVICE:verizon-service VERSION:dd37a91287259894ab092aa255b779479355d84f

--- terraform/dev/us-east-1/_frontend_apps/ui-cluster/verizon-service/variables.tf
+++ terraform/dev/us-east-1/_frontend_apps/ui-cluster/verizon-service/variables.tf
@@ -1,4 +1,4 @@
-variable "version" { default = "cd174beb1fb641413388b0717b141ca0ea09874c" }
+variable "version" { default = "dd37a91287259894ab092aa255b779479355d84f" }
 
 variable "log_level" {
   default = "info"
