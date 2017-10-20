e64e998023ba552fc09fd6f946f27f8ca5f6dbad
Jenkins <sunsystemsengineeringgroup@weather.com>
2017-10-20 06:11:49 +0000

    Update Image ENV:qa REGION:us-west-2 SERVICE:convo-api-service VERSION:6c5fdb229d93208fd2c95148ef511f98e5d646df

--- terraform/qa/us-west-2/_frontend_apps/ui-cluster/convo-api-service/variables.tf
+++ terraform/qa/us-west-2/_frontend_apps/ui-cluster/convo-api-service/variables.tf
@@ -1,4 +1,4 @@
-variable "app_version" { default = "40e9d897d4a0f399e3c3935f43d3dcb89d8989d8" }
+variable "app_version" { default = "6c5fdb229d93208fd2c95148ef511f98e5d646df" }
 
 variable "config_version" { default = "d5503e0bb0b0f7d539d799f08ae7700585382266" }
 
