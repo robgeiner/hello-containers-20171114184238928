09f8e6c93af85d61ba9dfd7e79936b13dfadcbef
Jenkins <sunsystemsengineeringgroup@weather.com>
2017-10-27 03:53:35 +0000

    Update Image ENV:prod REGION:us-east-1 SERVICE:hulu-service VERSION:098e0decab03866680d1caf3c69b5cf9a943634c

--- terraform/prod/us-east-1/_frontend_apps/ui-cluster/hulu-service/variables.tf
+++ terraform/prod/us-east-1/_frontend_apps/ui-cluster/hulu-service/variables.tf
@@ -1,4 +1,4 @@
-variable "version" { default = "0c7ecd3d2fe7fac51e7e226067654b58b1192412" }
+variable "version" { default = "098e0decab03866680d1caf3c69b5cf9a943634c" }
 
 variable "log_level" {
   default = "info"
